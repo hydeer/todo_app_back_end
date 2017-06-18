b533b98d7cb7c81cc608b5e9237f6eadad7f0f1c
hydeer <hy.lavarez.enrica@leadps.org>
2017-06-18 18:18:30 +0000

    Add list model and new migration file

--- db/migrate/20170618181513_create_todos.rb
+++ db/migrate/20170618181513_create_todos.rb
@@ -3,8 +3,8 @@ class CreateTodos < ActiveRecord::Migration
     create_table :todos do |t|
       t.string :description
       t.datetime :due_date
-      t.string :class
-      t.integer :time_estimate_seconds
+      t.string :subject
+      t.integer :time_estimate_min
       t.boolean :is_complete
 
       t.timestamps null: false
