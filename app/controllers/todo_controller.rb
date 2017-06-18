73e220ffba2c79f5cc6730ba57bf9b84582ab68f
hydeer <hy.lavarez.enrica@leadps.org>
2017-06-18 21:21:44 +0000

    Add conditional logic if statements to the show action of the Todo controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,11 +2,23 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        todo_id = params[:id]
+        
+        if todo_id == '1'
         @todo_description = "Complete Project"
         @todo_subject = "Biology"
         @todo_time_estimate_min = 120
         @todo_is_complete = true
-        @todo_due_date = 20171625
+        @todo_due_date = 20170625
+        
+        elsif todo_id == '2'
+        @todo_description = "Finish Analysis of Poem"
+        @todo_subject = "English"
+        @todo_time_estimate_min = 60
+        @todo_is_complete = true
+        @todo_due_date = 20170627
+        
+    end
     end
     def congratulations
     end 
