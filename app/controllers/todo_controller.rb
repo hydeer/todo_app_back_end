f5812f47fb4491571f63e1d3174c1bbb004954ea
hydeer <hy.lavarez.enrica@leadps.org>
2017-06-18 03:21:59 +0000

    ???

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,10 @@
 class TodoController < ApplicationController
     def index
     end
+    def show
+    end
+    def congratulations
+    end 
+    def home
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get 'todo/index', to: 'todo#index'
+  get 'todo/show', to: 'todo#show'
+  get 'todo/congratulations', to: 'todo#congratulations'
+  get 'todo/home', to: 'todo#home'
 end
